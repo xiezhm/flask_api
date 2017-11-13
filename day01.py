43891cad10b11c915ad33ebd0919cc80c75c55c8
475958179@qq.com <475958179@qq.com>
2017-11-13 15:09:21 +0800

    新文件：    .idea/flask.iml 	新文件：    .idea/modules.xml 	新文件：    .idea/workspace.xml 	新文件：    README.md 	新文件：    __init__.py 	新文件：    day01.py 	新文件：    static/ccs/index.ccs 	新文件：    templates/index.html 	新文件：    test02/__init__.py 	新文件：    test02/myflask.py 	新文件：    test02/templates/404.html 	新文件：    test02/templates/__init__.py

---
+++ day01.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# _*_conding:utf-8_*_
+from flask import Flask,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(port=80,debug=True)
---
+++ test02/myflask.py
@@ -0,0 +1,28 @@
+# _*_enconding:utf-8_*_
+from flask import Flask,request,render_template,json
+
+app = Flask(__name__)
+
+#验证key和key_src的值是和数据中的匹配，并返回对应的值
+def Verification(data):
+    key = '111'
+    key_src = '12345'
+    if data.get("key") == key and data.get("key_src") == key_src:
+        return json.dumps(data["data"])
+    else:
+        return "你的key和key_src值不对，请检查！"
+
+
+@app.route('/',methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        data = request.get_data()
+        data_s = json.loads(data)
+        s = Verification(data_s)
+        print(s)
+        return s
+    else:
+        return render_template('404.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0",port=88)
